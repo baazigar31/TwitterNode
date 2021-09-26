509ad3a93a02777b3fe6668f7cb81fe0dae6bde4
baazigar31 <imanand731@gmail.com>
2021-09-26 14:10:38 +0530

    Added db config

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express= require('express');
 const router= require('./src/routes/index');
+const connect= require('./src/config/database');
+
 var expressLayouts = require('express-ejs-layouts');
 
 
 
 const app =express();
-
+app.use(express.static('./src/assets'));
 app.use(expressLayouts);
+
 // app.set('layout',__dirname+'/src/views/layouts/layout')
 app.use('/',router);
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.listen(3000,()=>{
+app.listen(3000,async ()=>{
+    await connect(); 
     console.log('Server started at 3000!!');
 })
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 module.exports.profile= function(req,res){
     // res.send('<h1>User Profile</h1>');
     // return res.render('./users/user_profile',{title:"User Profile",layout:false});
-    return res.render('./users/user_profile',{title:"User Profile",layout:'user_layout'}); // by deaflut layout will look in views for the layout
+    return res.render('./users/user_profile',{title:"User Profile",layout:'user_layout'}); // by deaflut layout will look in views for the layout 
 }
