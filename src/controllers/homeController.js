ae1e5fefe989d130a5ed37f022b2cfa74ed9e3e3
baazigar31 <imanand731@gmail.com>
2021-09-29 12:09:21 +0530

    Added tweet display

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,17 @@
 const ejs =require('ejs');
+const Tweet = require('../models/tweet');
 
 module.exports.root = function(req,res) {
     // return res.send('<h1>Welcome to twitter</h1>');
-    return res.render('home',{title:"Twitter",pageName:"Twitter Home Page",layout:'layout'});
+
+     Tweet.find({}).populate('user').exec(function(err,tweets){
+      let fetchedTweets = tweets;
+      console.log(tweets);
+      if(err){
+        console.log('Error finding tweets');
+        fetchedTweets = {};
+      }
+      return res.render('home',{title:"Twitter",tweets:fetchedTweets,layout:'layout'});
+    });
+    
 }
--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const {create} = require('../controllers/tweetController');
+const passport = require('passport');
 
 const router = express.Router();
 
-router.post('/create', create);
+router.post('/create',passport.checkAuthentication, create);
 
 module.exports = router; 
