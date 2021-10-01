23605a8e6739d2e9b4a044bfbb44e08bfb8e34aa
baazigar31 <imanand731@gmail.com>
2021-10-01 21:16:21 +0530

    Delete feature added for tweets

--- src/controllers/tweetController.js
+++ src/controllers/tweetController.js
@@ -1,4 +1,6 @@
 const Tweet = require('../models/tweet');
+const Comment = require('../models/comment');
+
 
 const create = function(req, res) {
     console.log(req);
@@ -14,4 +16,22 @@ const create = function(req, res) {
     })
 }
 
-module.exports = {create};
+const destroy = function(req,res){
+    console.log('hit');
+    Tweet.findById(req.params.id, function(err,tweet){
+        if(err){
+            return res.redirect('/');
+        }
+        if(tweet.user == req.user.id){
+            tweet.remove();
+            Comment.deleteMany({tweet: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            res.redirect('back');
+        }
+    })
+}
+
+module.exports = {create, destroy};
--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {create} = require('../controllers/tweetController');
+const {create, destroy} = require('../controllers/tweetController');
 const passport = require('passport');
 
 const router = express.Router();
 
 router.post('/create',passport.checkAuthentication, create);
+router.get('/destroy/:id',passport.checkAuthentication,destroy);
 
 module.exports = router; 
